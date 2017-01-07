7fad07410e2b925c84884760b67b2460dd71c75e
Olivier Maillot <maillot.olivier@gmail.com>
2017-01-08 02:06:29 +0400

    Correct typo

--- recursive-permutation.js
+++ recursive-permutation.js
@@ -1,6 +1,6 @@
 //Recursion & permutation (FR)
 function doPerm(num) {
-  //var global pour stocker le resultat des différentes recursions
+  //var global pour stocker le résultat des différentes recursions
   var results = [];
   
   //passe d'un int à un string puis à un array
@@ -14,33 +14,33 @@ function doPerm(num) {
     var cur; memo = memo || [];
     n++;
     
-    //boucle d'itération pour fixer le curseur sur un chiffre puis
+    // boucle d'itération pour fixer le curseur sur un chiffre puis
     // on permute les autres chiffres.
-    // ex : 123 => 1 23 et 1 32 (1 en première position sur memo)
-    //      puis 2 13 et 2 31,  (2 en prem pos sur memo)
-    //      puis 3 12 et 3 21.  (3 en prem pos sur memo)
+    // ex : 123 => 1 23 et 1 32 (1 en première position sur la var "memo")
+    //      puis 2 13 et 2 31,  (puis 2)
+    //      puis 3 12 et 3 21.  (enfin 3)
     for (var i = 0; i < arr.length; i++) {
       cur = arr.splice(i, 1);
       
-      // lorsque l'on a tous les chiffres dans le memo,
+      // lorsque l'on a tous les chiffres contenus 
+      // dans notre variable memo,
       // cela veut dire que l'on a une permutation, 
       // on l'enregistre donc dans l'array results.
       if (arr.length === 0) {
         results.push(memo.concat(cur).join(''));
       }
-      //aide pour decripter le fonctionnement de la recursion.
+      // petite aide pour decrypter/debugger le fonctionnement de la recursion.
       console.log(">>> permute("+arr.slice()+";"+memo.concat(cur)+")");
       
-      //Recursion
+      // RECURSION
       permute(arr.slice(), memo.concat(cur));
       
       // permet de permuter les chiffres dans l'array
-      // reinsert à l'emplacement i de l'array le dernier chiffre retirer
-      // /!\ recursion 
+      // reinsert à l'emplacement i de l'array le dernier chiffre retiré
+      // /!\ recursion /!\
       arr.splice(i, 0, cur[0]);
     }
     return results;
   }
-
   return permute(numToArr);
 }
